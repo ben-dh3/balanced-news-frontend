fcfe6496d3aeecf46eb04e24abf40bb71f7bbe9c
ben-dh3 <benjaminduffield1997@gmail.com>
2025-03-21 10:56:54 +0000

    add pull to refresh to articles page

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -4,15 +4,22 @@ import SummaryTabs from '../components/SummaryTabs';
 import ArticleList from '../components/ArticleList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import PullToRefresh from '@/components/PullToRefresh';
+import { useQueryClient } from '@tanstack/react-query';
 
 const Articles: React.FC = () => {
   const { storyId } = useParams<{ storyId: string }>();
   const navigate = useNavigate();
   const location = useLocation();
   const { title, imageUrl } = location.state || {};
+  const queryClient = useQueryClient();
 
   const handleGoBack = () => {
     navigate('/');
+  };
+
+  const handleRefresh = async () => {
+    await queryClient.invalidateQueries({ queryKey: ['articles', 'summaries'] });
   };
   
   const { data: summaryData, isLoading: summariesLoading } = useSummaryData(storyId);
@@ -21,25 +28,29 @@ const Articles: React.FC = () => {
   if (summariesLoading || articlesLoading) return <div>Loading article data...</div>;
 
   return (
-    <div className="m-4 space-y-4 font-display">
-      <button onClick={handleGoBack} className="cursor-pointer w-10 h-10 bg-primary-100 rounded-full">
-        <FontAwesomeIcon icon={faArrowLeft} />
-      </button>
+    <PullToRefresh onRefresh={handleRefresh}>
+      <div className="m-4 space-y-4 font-display">
+        <button onClick={handleGoBack} className="cursor-pointer w-10 h-10 bg-primary-100 rounded-full">
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
 
-      {title && imageUrl && (
-        <div className="flex justify-between">
-          <h2 className="text-2xl font-title font-black">{title}</h2>
-          <img 
-          src={imageUrl}
-          className="w-28 h-28 object-cover rounded-xl"
-          alt={title}
-          />
-        </div>
-      )}
-      
-      <SummaryTabs summaryData={summaryData} />
-      <ArticleList articles={articlesData?.articles || {}} />
-    </div>
+        {title && imageUrl && (
+          <div className="flex justify-between">
+            <h2 className="text-2xl font-title font-black">{title}</h2>
+            <img 
+            src={imageUrl}
+            className="w-28 h-28 object-cover rounded-xl"
+            alt={title}
+            />
+          </div>
+        )}
+        
+        <SummaryTabs summaryData={summaryData} />
+        <ArticleList articles={articlesData?.articles || {}} />
+      </div>
+
+    </PullToRefresh>
+   
   );
 };
 
