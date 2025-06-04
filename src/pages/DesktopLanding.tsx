b7de29879b820fc646b4dbbf029885f963db8e30
ben-dh3 <benjaminduffield1997@gmail.com>
2025-06-04 11:01:36 +0100

    update landing page text

--- src/pages/DesktopLanding.tsx
+++ src/pages/DesktopLanding.tsx
@@ -3,8 +3,7 @@ const DesktopLanding = () => {
       <div className="space-y-4 flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-white p-6">
         <img className="self-center w-60" src="/title.svg" alt='Balanced News Title'></img>
         <p className="font-display text-lg text-center mb-6">
-          This application is optimized for mobile devices.
-          Please access it from your phone for the best experience.
+        Download the mobile app or view it right here in your browser by resizing this window to mobile size.
         </p>
         <div className="bg-white p-4 rounded-lg shadow-lg">
           <img src="/logo.png" alt="Balanced News Logo" className="w-20 h-20" />
